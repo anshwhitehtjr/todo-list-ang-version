dd343a138795774ef29708eabcf3b744f5571e7a
anshwhitehtjr <kalpaiteansh@gmail.com>
2021-12-08 21:29:39 +0530

    First Angular App First InComplete APp

---
+++ src/app/MyComponent/todos/todos.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+  todos: Todo[];
+  constructor () {
+    this.todos = [];
+  }
+
+  ngOnInit(): void { }
+
+  deleteTodo(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    this.todos.splice(index, 1);
+  }
+
+  addTodo(todo: Todo) {
+    this.todos.push(todo);
+  }
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent
+{
   title = 'cwh-todo-list';
+  constructor ()
+  {
+    // setTimeout(() =>
+    // {
+    //   this.title = "Changed Title";
+    // }, 2000);
+    
+  }
 }
